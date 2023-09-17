66c68b2974213fca9f69168d1ec9232818a7fdbd
Bea Steers <bea.steers@gmail.com>
2023-09-17 11:41:03 -0400

    simplify

--- tests/monitor.py
+++ tests/monitor.py
@@ -1,62 +1,6 @@
-import redis        
-
-class Monitor:
-    def __init__(self, connection_pool):
-        self.connection_pool = connection_pool
-        self.connection = None
-
-    def __del__(self):
-        try:
-            self.reset()
-        except:
-            pass
-
-    def reset(self):
-        if self.connection:
-            self.connection_pool.release(self.connection)
-            self.connection = None
-
-    def monitor(self):
-        if self.connection is None:
-            self.connection = self.connection_pool.get_connection(
-                'monitor', None)
-        self.connection.send_command("monitor")
-        return self.listen()
-
-    def parse_response(self):
-        msg = self.connection.read_response()
-        return 
-
-    def listen(self):
-        while True:
-            yield self.parse_response()
-
-
-
-
-
-class Monitor:
-    def __init__(self, r):
-        self.r = r
-
-    def run(self):
-        STATE = "waiting"
-        with r.monitor() as m:
-            for command in m.listen():
-                if STATE == "waiting":
-                    pass
-                if STATE == "waiting":
-                    pass
-
+import redis
 
 if  __name__ == '__main__':
-    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
-    # monitor = Monitor(pool)
-    # commands = monitor.monitor()
-
-    # for c in commands:
-    #     print(c)
-
     r = redis.Redis(host='localhost', port=6379, db=0)
     with r.monitor() as m:
         for command in m.listen():
